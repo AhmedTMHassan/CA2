7c41a527f72367d8a3acf17f4458f51262bef69f
Ahmed <X00210493@myTUDublin.ie>
2024-11-12 14:50:11 +0000

    CA2

--- cars_project/urls.py
+++ cars_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('base_page/', include('django.contrib.auth.urls')),
     path('base_page/', include('base_page.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('base_page.urls')),
 ]
