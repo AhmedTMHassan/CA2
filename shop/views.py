704697a9b427e82f28f9bf374d327f72a7c92911
Ahmed <X00210493@myTUDublin.ie>
2024-11-30 23:18:57 +0000

    CA2

--- shop/views.py
+++ shop/views.py
@@ -31,9 +31,47 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'shop/prod_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['make_list'] = Make.objects.all()  # Pass makes to the context
+        return context
+
+    def get_queryset(self):
+        queryset = Product.objects.all() 
+        make_name = self.kwargs.get('make_name')  
+        
+        if not make_name:
+            make_name = self.request.GET.get('make')  # Get make from query parameter (from the form)
+        
+        # If make_name is provided and not "All Makes", filter products by make
+        if make_name and make_name != "All Makes":
+            make = get_object_or_404(Make, name=make_name)
+            queryset = queryset.filter(make=make) 
+
+        return queryset
+
 
 class MakeListView(ListView):
     model = Make
     context_object_name = 'make_list'
     template_name = 'shop/make_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['make_list'] = Make.objects.all()  # Pass makes to the context
+        return context
+
+    def get_queryset(self):
+        queryset = Product.objects.all() 
+        make_name = self.kwargs.get('make_name')  
+        
+        if not make_name:
+            make_name = self.request.GET.get('make')  # Get make from query parameter (from the form)
+        
+        # If make_name is provided and not "All Makes", filter products by make
+        if make_name and make_name != "All Makes":
+            make = get_object_or_404(Make, name=make_name)
+            queryset = queryset.filter(make=make) 
+
+        return queryset
 
